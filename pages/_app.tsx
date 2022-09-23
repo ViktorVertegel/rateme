e68ef5b8f07cfb9dab6e7576fde2bfb17b553598
ViktorVertegel <vertegel@gmail.com>
2022-09-23 16:35:15 +0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+     <Head>
+        <title>Рейтинг чегототам</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous'/>
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
+      </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
