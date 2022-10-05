4d74f5f5f0f3e2df148afc8d7b12edfc63730176
ViktorVertegel <vertegel@gmail.com>
2022-10-05 18:26:35 +0300

    Added rating comp

--- next-env-custom.d.ts
+++ next-env-custom.d.ts
@@ -1,5 +1,9 @@
 /// <reference types="next" />
+/// <reference types="next/types/global" />
 
-
+declare module "*.svg" {
+	const content: React.FunctionComponent<React.SVGAttributes<SVGAElement>>;
+	export default content;
+}
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
