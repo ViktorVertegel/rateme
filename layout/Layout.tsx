949a8cdf57d613e2696b071247cb0eb45f2ea7ac
ViktorVertegel <vertegel@gmail.com>
2022-10-21 18:40:39 +0300

    dfsdf

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,13 +1,37 @@
 import { LayoutProps } from "./Layout.props";
+
 import styles from './Layout.module.css';
 import cn from 'classnames';
+import { Header } from "./Header/Header";
+import { Sidebar } from "./Sidebar/Sidebar";
+import { Footer } from "./Footer/Footer";
+import { Component, FunctionComponent } from "react";
 
 
-export const Layout = ({ children }: LayoutProps): JSX.Element => {
+
+
+const Layout = ({ children }: LayoutProps): JSX.Element => {
     return (
-        <></>
+        <>
+            <Header />
+            <div>
+                <Sidebar />
+                <div>
+                    {children}
+                </div>
+            </div>
+            <Footer />
+        </>
     )
 
 }   
 
-
+export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+    return function withLayoutComponent(props: T): JSX.Element {
+        return (
+            <Layout>
+                <Component {...props} />
+            </Layout>
+        )
+    }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Button, Htag, P, Rating, Tag } from "../components";
+import { withLayout } from "../layout/Layout";
 
-
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   
   const [rating, setRating] = useState<number>(4);
 
   return (
-    <div>
+    <>
       <Htag tag='h1'>{rating}</Htag>
       <Button appearance="primary" arrow="right" >Кнопка</Button>
       <Button appearance="ghost" arrow='down'>Кнопка Вторая</Button>
@@ -17,6 +17,8 @@ export default function Home(): JSX.Element {
       <Tag size='s' color="green">knlsfjklfjkldfs</Tag>
       <Tag size='m' color="gray" href="google.com">knlklfjkldfs</Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
-    </div>
+    </>
   );
 };
+
+export default withLayout(Home); 
